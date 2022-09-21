f0a961acb19a5958c8c9d36650544f1d1ca108da
joshua <jrbauti1@asu.edu>
2022-09-21 11:56:12 -0700

    Updates

---
+++ precache-manifest.d7856f13b3c80622e57f76dd02a3723a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af890c10f1c48273ebba2e026b412650",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "a8cc96e289fe2d182b1c",
+    "url": "/react-chess/static/css/main.607c8601.chunk.css"
+  },
+  {
+    "revision": "f0307cd8b541b2ce0168",
+    "url": "/react-chess/static/js/2.58fed9dd.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/react-chess/static/js/2.58fed9dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8cc96e289fe2d182b1c",
+    "url": "/react-chess/static/js/main.b9863f6c.chunk.js"
+  },
+  {
+    "revision": "2fb2450852e97fdac306",
+    "url": "/react-chess/static/js/runtime-main.c36c0db2.js"
+  },
+  {
+    "revision": "3382a95341b72c229183dcc0f1168176",
+    "url": "/react-chess/static/media/q_w.3382a953.png"
+  }
+]);
